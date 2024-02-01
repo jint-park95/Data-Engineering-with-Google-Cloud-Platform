4823c3efd22c528979f9fd18a23f69e32f0f66c8
Jintae Park <jint.park95@gmail.com>
2024-02-01 05:11:53 +0000

    cloud composer WIP

--- chapter-4/code/jtp_level_2_dag.py
+++ chapter-4/code/jtp_level_2_dag.py
@@ -5,5 +5,63 @@ from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.utils.dates import days_ago
 
 args = {
-    'owner': 'jtp',
+    'owner': 'jtp'
 }
+
+GCP_PROJECT_ID = 'jtp-gcp-data-eng'
+INSTANCE_NAME = 'mysql-instance-source'
+EXPORT_URI = 'gs://jtp-gcp-data-eng-data-bucket/mysql_export/from_composer/stations/stations.csv'
+SQL_QUERY = "SELECT * FROM apps_db.stations"
+
+export_body = {
+    "exportContext": {
+        "fileType": "csv",
+        "uri": EXPORT_URI,
+        "csvExportOptions":{
+            "selectQuery": SQL_QUERY
+        }
+    }
+}
+
+with DAG(
+    dag_id='level_2_dag_load_bigquery',
+    default_args=args,
+    schedule_interval='0 5 * * *',
+    start_date=days_ago(1),
+) as dag:
+
+    sql_export_task = CloudSqlInstanceExportOperator(
+        project_id=GCP_PROJECT_ID, 
+        body=export_body, 
+        instance=INSTANCE_NAME, 
+        task_id='sql_export_task'
+    )
+
+    gcs_to_bq_example = GoogleCloudStorageToBigQueryOperator(
+    task_id                             = "gcs_to_bq_example",
+    bucket                              = 'jtp-gcp-data-eng-data-bucket',
+    source_objects                      = ['mysql_export/from_composer/stations/stations.csv'],
+    destination_project_dataset_table   ='raw_bikesharing.stations',
+    schema_fields=[
+        {'name': 'station_id', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'region_id', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'capacity', 'type': 'INTEGER', 'mode': 'NULLABLE'}
+    ],
+    write_disposition='WRITE_TRUNCATE'
+    )
+
+    bq_to_bq  = BigQueryOperator(
+        task_id                     = "bq_to_bq",
+        sql                         = "SELECT count(*) as count FROM `raw_bikesharing.stations`",
+        destination_dataset_table   = 'dwh_bikesharing.temporary_stations_count',
+        write_disposition           = 'WRITE_TRUNCATE',
+        create_disposition          = 'CREATE_IF_NEEDED',
+        use_legacy_sql              = False,
+        priority                    = 'BATCH'
+    )
+
+    sql_export_task >> gcs_to_bq_example >> bq_to_bq
+
+if __name__ == "__main__":
+    dag.cli()
